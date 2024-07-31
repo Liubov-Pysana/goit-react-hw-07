ba00a8504d84db874ccdd3f5f319a263d625ff7f
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-07-31 17:00:01 +0100

    settuped api, edit css

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASE_URL = "https://66a7e0a953c13f22a3d1573e.mockapi.io";
+
+// Fetch contacts
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/contacts`);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
+
+// Add a new contact
+export const addContact = createAsyncThunk("contacts/addContact", async (contact, thunkAPI) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/contacts`, contact);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
+
+// Delete a contact by ID
+export const deleteContact = createAsyncThunk("contacts/deleteContact", async (id, thunkAPI) => {
+    try {
+        await axios.delete(`${BASE_URL}/contacts/${id}`);
+        return id;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
